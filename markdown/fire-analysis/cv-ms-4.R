b40e1dd494da656735ffdd9293a686a56a693a00
Sam Morris <smorris@uwlax.edu>
2016-02-02 10:21:19 -0600

    change names to kern instead of ms

--- markdown/fire-analysis/cv-ms-4.R
+++ markdown/fire-analysis/cv-ms-4.R
@@ -5,7 +5,7 @@ source(file = "./package_load.R", chdir = T)
 # Number of bases: In this method we are combaring the performance of the abba
 # function to our proposed method which uses basis functions. So, here
 # there aren't a set of basis functions.
-L <- "ms" # using ms for the results from abba
+L <- "kern" # using ms for the results from abba
 cv <- 4   # which cross-validation set to use
 results.file <- paste("./cv-results/", L, "-", cv, ".RData", sep = "")
 table.file   <- paste("./cv-tables/", L, "-", cv, ".txt", sep = "")
@@ -23,7 +23,7 @@ source(file = "./package_load.R", chdir = T)
 # fact that they need to add up to 1 across all basis functions at each site.
 # opting for 2, 5, 10, 15, and then looking a max stable method with fixed 
 # alpha.
-L <- "ms" # number of basis functions
+L <- "kern" # number of basis functions
 cv <- 9   # which cross-validation set to use
 results.file <- paste("./cv-results/", L, "-", cv, ".RData", sep = "")
 table.file   <- paste("./cv-tables/", L, "-", cv, ".txt", sep = "")

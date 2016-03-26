61b0edfc72b6fa3f5787dd55d1022b6803f2d3f7
Sam Morris <sam.morris81@gmail.com>
2016-03-26 15:57:53 -0500

    add in second round launch

---
+++ markdown/fire-analysis/launch2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# CWD=`pwd`
+bwsubmit r ebf-ebf-25-1.R
+bwsubmit r ebf-ebf-25-2.R
+bwsubmit r ebf-ebf-25-3.R
+bwsubmit r ebf-ebf-25-4.R
+bwsubmit r ebf-ebf-25-5.R
+bwsubmit r ebf-gsk-25-1.R
+bwsubmit r ebf-gsk-25-2.R
+bwsubmit r ebf-gsk-25-3.R
+bwsubmit r ebf-gsk-25-4.R
+bwsubmit r ebf-gsk-25-5.R
+bwsubmit r gsk-gsk-all-1.R
+bwsubmit r gsk-gsk-all-2.R
+bwsubmit r gsk-gsk-all-3.R
+bwsubmit r gsk-gsk-all-4.R
+bwsubmit r gsk-gsk-all-5.R
+done

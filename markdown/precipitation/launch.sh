bb3cb3d433a77365bb4a6732ab5bfd0eeb68e450
Sam Morris <smorris@uwlax.edu>
2016-04-28 11:12:26 -0500

    adding in L = 35, 40

--- markdown/precipitation/launch.sh
+++ markdown/precipitation/launch.sh
@@ -1,63 +1,83 @@
 #!/bin/bash
 # CWD=`pwd`
-bwsubmit r fit-ebf-5-1.R
-bwsubmit r fit-ebf-5-2.R
-bwsubmit r fit-ebf-5-3.R
-bwsubmit r fit-ebf-5-4.R
-bwsubmit r fit-ebf-5-5.R
-bwsubmit r fit-ebf-10-1.R
-bwsubmit r fit-ebf-10-2.R
-bwsubmit r fit-ebf-10-3.R
-bwsubmit r fit-ebf-10-4.R
-bwsubmit r fit-ebf-10-5.R
-bwsubmit r fit-ebf-15-1.R
-bwsubmit r fit-ebf-15-2.R
-bwsubmit r fit-ebf-15-3.R
-bwsubmit r fit-ebf-15-4.R
-bwsubmit r fit-ebf-15-5.R
-bwsubmit r fit-ebf-20-1.R
-bwsubmit r fit-ebf-20-2.R
-bwsubmit r fit-ebf-20-3.R
-bwsubmit r fit-ebf-20-4.R
-bwsubmit r fit-ebf-20-5.R
-bwsubmit r fit-ebf-25-1.R
-bwsubmit r fit-ebf-25-2.R
-bwsubmit r fit-ebf-25-3.R
-bwsubmit r fit-ebf-25-4.R
-bwsubmit r fit-ebf-25-5.R
+# bwsubmit r fit-ebf-5-1.R
+# bwsubmit r fit-ebf-5-2.R
+# bwsubmit r fit-ebf-5-3.R
+# bwsubmit r fit-ebf-5-4.R
+# bwsubmit r fit-ebf-5-5.R
+# bwsubmit r fit-ebf-10-1.R
+# bwsubmit r fit-ebf-10-2.R
+# bwsubmit r fit-ebf-10-3.R
+# bwsubmit r fit-ebf-10-4.R
+# bwsubmit r fit-ebf-10-5.R
+# bwsubmit r fit-ebf-15-1.R
+# bwsubmit r fit-ebf-15-2.R
+# bwsubmit r fit-ebf-15-3.R
+# bwsubmit r fit-ebf-15-4.R
+# bwsubmit r fit-ebf-15-5.R
+# bwsubmit r fit-ebf-20-1.R
+# bwsubmit r fit-ebf-20-2.R
+# bwsubmit r fit-ebf-20-3.R
+# bwsubmit r fit-ebf-20-4.R
+# bwsubmit r fit-ebf-20-5.R
+# bwsubmit r fit-ebf-25-1.R
+# bwsubmit r fit-ebf-25-2.R
+# bwsubmit r fit-ebf-25-3.R
+# bwsubmit r fit-ebf-25-4.R
+# bwsubmit r fit-ebf-25-5.R
 # bwsubmit r fit-ebf-30-1.R
 # bwsubmit r fit-ebf-30-2.R
 # bwsubmit r fit-ebf-30-3.R
 # bwsubmit r fit-ebf-30-4.R
 # bwsubmit r fit-ebf-30-5.R
-bwsubmit r fit-gsk-5-1.R
-bwsubmit r fit-gsk-5-2.R
-bwsubmit r fit-gsk-5-3.R
-bwsubmit r fit-gsk-5-4.R
-bwsubmit r fit-gsk-5-5.R
-bwsubmit r fit-gsk-10-1.R
-bwsubmit r fit-gsk-10-2.R
-bwsubmit r fit-gsk-10-3.R
-bwsubmit r fit-gsk-10-4.R
-bwsubmit r fit-gsk-10-5.R
-bwsubmit r fit-gsk-15-1.R
-bwsubmit r fit-gsk-15-2.R
-bwsubmit r fit-gsk-15-3.R
-bwsubmit r fit-gsk-15-4.R
-bwsubmit r fit-gsk-15-5.R
-bwsubmit r fit-gsk-20-1.R
-bwsubmit r fit-gsk-20-2.R
-bwsubmit r fit-gsk-20-3.R
-bwsubmit r fit-gsk-20-4.R
-bwsubmit r fit-gsk-20-5.R
-bwsubmit r fit-gsk-25-1.R
-bwsubmit r fit-gsk-25-2.R
-bwsubmit r fit-gsk-25-3.R
-bwsubmit r fit-gsk-25-4.R
-bwsubmit r fit-gsk-25-5.R
+bwsubmit r fit-ebf-35-1.R
+bwsubmit r fit-ebf-35-2.R
+bwsubmit r fit-ebf-35-3.R
+bwsubmit r fit-ebf-35-4.R
+bwsubmit r fit-ebf-35-5.R
+bwsubmit r fit-ebf-40-1.R
+bwsubmit r fit-ebf-40-2.R
+bwsubmit r fit-ebf-40-3.R
+bwsubmit r fit-ebf-40-4.R
+bwsubmit r fit-ebf-40-5.R
+# bwsubmit r fit-gsk-5-1.R
+# bwsubmit r fit-gsk-5-2.R
+# bwsubmit r fit-gsk-5-3.R
+# bwsubmit r fit-gsk-5-4.R
+# bwsubmit r fit-gsk-5-5.R
+# bwsubmit r fit-gsk-10-1.R
+# bwsubmit r fit-gsk-10-2.R
+# bwsubmit r fit-gsk-10-3.R
+# bwsubmit r fit-gsk-10-4.R
+# bwsubmit r fit-gsk-10-5.R
+# bwsubmit r fit-gsk-15-1.R
+# bwsubmit r fit-gsk-15-2.R
+# bwsubmit r fit-gsk-15-3.R
+# bwsubmit r fit-gsk-15-4.R
+# bwsubmit r fit-gsk-15-5.R
+# bwsubmit r fit-gsk-20-1.R
+# bwsubmit r fit-gsk-20-2.R
+# bwsubmit r fit-gsk-20-3.R
+# bwsubmit r fit-gsk-20-4.R
+# bwsubmit r fit-gsk-20-5.R
+# bwsubmit r fit-gsk-25-1.R
+# bwsubmit r fit-gsk-25-2.R
+# bwsubmit r fit-gsk-25-3.R
+# bwsubmit r fit-gsk-25-4.R
+# bwsubmit r fit-gsk-25-5.R
 # bwsubmit r fit-gsk-30-1.R
 # bwsubmit r fit-gsk-30-2.R
 # bwsubmit r fit-gsk-30-3.R
 # bwsubmit r fit-gsk-30-4.R
 # bwsubmit r fit-gsk-30-5.R
+bwsubmit r fit-gsk-35-1.R
+bwsubmit r fit-gsk-35-2.R
+bwsubmit r fit-gsk-35-3.R
+bwsubmit r fit-gsk-35-4.R
+bwsubmit r fit-gsk-35-5.R
+bwsubmit r fit-gsk-40-1.R
+bwsubmit r fit-gsk-40-2.R
+bwsubmit r fit-gsk-40-3.R
+bwsubmit r fit-gsk-40-4.R
+bwsubmit r fit-gsk-40-5.R
 done

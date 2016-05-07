7fea48011d96a5b051287c22649b2f2f51222f73
Sam Morris <sam.morris81@gmail.com>
2016-05-07 15:49:40 -0500

    launching all files through exec on BW

--- markdown/precipitation/launch.sh
+++ markdown/precipitation/launch.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # CWD=`pwd`
-bwsubmit r fit-ebf-5-1.R
-bwsubmit r fit-ebf-5-2.R
-bwsubmit r fit-ebf-5-3.R
-bwsubmit r fit-ebf-5-4.R
-bwsubmit r fit-ebf-5-5.R
-bwsubmit r fit-ebf-10-1.R
-bwsubmit r fit-ebf-10-2.R
-bwsubmit r fit-ebf-10-3.R
-bwsubmit r fit-ebf-10-4.R
-bwsubmit r fit-ebf-10-5.R
-bwsubmit r fit-ebf-15-1.R
-bwsubmit r fit-ebf-15-2.R
-bwsubmit r fit-ebf-15-3.R
-bwsubmit r fit-ebf-15-4.R
-bwsubmit r fit-ebf-15-5.R
-bwsubmit r fit-ebf-20-1.R
-bwsubmit r fit-ebf-20-2.R
-bwsubmit r fit-ebf-20-3.R
-bwsubmit r fit-ebf-20-4.R
-bwsubmit r fit-ebf-20-5.R
-bwsubmit r fit-ebf-25-1.R
-bwsubmit r fit-ebf-25-2.R
-bwsubmit r fit-ebf-25-3.R
-bwsubmit r fit-ebf-25-4.R
-bwsubmit r fit-ebf-25-5.R
-bwsubmit r fit-ebf-30-1.R
-bwsubmit r fit-ebf-30-2.R
-bwsubmit r fit-ebf-30-3.R
-bwsubmit r fit-ebf-30-4.R
-bwsubmit r fit-ebf-30-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-5-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-5-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-5-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-5-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-5-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-10-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-10-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-10-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-10-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-10-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-15-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-15-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-15-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-15-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-15-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-20-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-20-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-20-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-20-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-20-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-25-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-25-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-25-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-25-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-25-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-30-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-30-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-30-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-30-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-ebf-30-5.R
 # bwsubmit r fit-ebf-35-1.R
 # bwsubmit r fit-ebf-35-2.R
 # bwsubmit r fit-ebf-35-3.R
@@ -40,36 +40,36 @@ bwsubmit r fit-ebf-30-5.R
 # bwsubmit r fit-ebf-40-3.R
 # bwsubmit r fit-ebf-40-4.R
 # bwsubmit r fit-ebf-40-5.R
-bwsubmit r fit-gsk-5-1.R
-bwsubmit r fit-gsk-5-2.R
-bwsubmit r fit-gsk-5-3.R
-bwsubmit r fit-gsk-5-4.R
-bwsubmit r fit-gsk-5-5.R
-bwsubmit r fit-gsk-10-1.R
-bwsubmit r fit-gsk-10-2.R
-bwsubmit r fit-gsk-10-3.R
-bwsubmit r fit-gsk-10-4.R
-bwsubmit r fit-gsk-10-5.R
-bwsubmit r fit-gsk-15-1.R
-bwsubmit r fit-gsk-15-2.R
-bwsubmit r fit-gsk-15-3.R
-bwsubmit r fit-gsk-15-4.R
-bwsubmit r fit-gsk-15-5.R
-bwsubmit r fit-gsk-20-1.R
-bwsubmit r fit-gsk-20-2.R
-bwsubmit r fit-gsk-20-3.R
-bwsubmit r fit-gsk-20-4.R
-bwsubmit r fit-gsk-20-5.R
-bwsubmit r fit-gsk-25-1.R
-bwsubmit r fit-gsk-25-2.R
-bwsubmit r fit-gsk-25-3.R
-bwsubmit r fit-gsk-25-4.R
-bwsubmit r fit-gsk-25-5.R
-bwsubmit r fit-gsk-30-1.R
-bwsubmit r fit-gsk-30-2.R
-bwsubmit r fit-gsk-30-3.R
-bwsubmit r fit-gsk-30-4.R
-bwsubmit r fit-gsk-30-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-5-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-5-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-5-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-5-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-5-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-10-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-10-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-10-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-10-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-10-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-15-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-15-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-15-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-15-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-15-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-20-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-20-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-20-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-20-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-20-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-25-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-25-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-25-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-25-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-25-5.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-30-1.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-30-2.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-30-3.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-30-4.R
+bwsubmit exec ~/packages/R/lib64/R/bin/Rscript fit-gsk-30-5.R
 # bwsubmit r fit-gsk-35-1.R
 # bwsubmit r fit-gsk-35-2.R
 # bwsubmit r fit-gsk-35-3.R

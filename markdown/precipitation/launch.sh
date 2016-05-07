b892a5427e80252f874396b3a06cbdf790035153
Sam Morris <sam.morris81@gmail.com>
2016-05-07 18:53:43 -0500

    edits to run with gsk too

--- markdown/precipitation/launch.sh
+++ markdown/precipitation/launch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # CWD=`pwd`
 
-for i in `seq 1 2`
+for i in `seq 1 7`
 do
-  for j in `seq 1 2`
+  for j in `seq 1 5`
   do
     L=$((i * 5))
     bwsubmit exec launch-ebf.sh $L $j
+    bwsubmit exec launch-gsk.sh $L $j
   done
 done
 exit 0

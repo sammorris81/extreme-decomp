9df55afae350144f8f73497c38263d012b9f2540
Sam Morris <sam.morris81@gmail.com>
2016-05-07 18:49:49 -0500

    bugfix

--- markdown/precipitation/launch-ebf.sh
+++ markdown/precipitation/launch-ebf.sh
@@ -5,6 +5,6 @@ PREC=$HOME/repos-git/extreme-decomp/markdown/precipitation
 L=$1
 CV=$2
 
-$RBIN CMD BATCH --vanilla --no-save $PREC/fit-ebf-L-CV.R $PREC/fit-ebf-L-CV.out 2>&1
+$RBIN CMD BATCH --vanilla --no-save $PREC/fit-ebf-$L-$CV.R $PREC/fit-ebf-$L-$CV.out 2>&1
 
 exit 0

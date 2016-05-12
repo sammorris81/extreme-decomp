2315e8c72c44833b4446353780ecaab137c493de
Sam Morris <smorris@uwlax.edu>
2016-05-12 00:41:20 -0500

    bugfix

--- markdown/fire-analysis/launch-gsk.sh
+++ markdown/fire-analysis/launch-gsk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # CWD=`pwd`
 RBIN=$HOME/packages/R/lib64/R/bin/R
-PREC=$HOME/repos-git/extreme-decomp/markdown/precipitation
+FIRE=$HOME/repos-git/extreme-decomp/markdown/fire-analysis
 L=$1
 CV=$2
 
-$RBIN CMD BATCH --vanilla --no-save $PREC/fit-gsk-$L-$CV.R $PREC/fit-gsk-$L-$CV.out 2>&1
+$RBIN CMD BATCH --vanilla --no-save $FIRE/fit-gsk-$L-$CV.R $FIRE/fit-gsk-$L-$CV.out 2>&1
 
 exit 0

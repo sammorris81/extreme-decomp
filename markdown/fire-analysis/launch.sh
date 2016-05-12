b397334da8fdb41fb159e8943cbba55359ae2c17
Sam Morris <smorris@uwlax.edu>
2016-05-11 23:08:49 -0500

    setting up a script to launch the files

--- markdown/fire-analysis/launch.sh
+++ markdown/fire-analysis/launch.sh
@@ -1,83 +1,13 @@
 #!/bin/bash
 # CWD=`pwd`
-# bwsubmit r ebf-gsk-5-1.R
-# bwsubmit r ebf-gsk-5-2.R
-# bwsubmit r ebf-gsk-5-3.R
-# bwsubmit r ebf-gsk-5-4.R
-# bwsubmit r ebf-gsk-5-5.R
-# bwsubmit r ebf-gsk-10-1.R
-# bwsubmit r ebf-gsk-10-2.R
-# bwsubmit r ebf-gsk-10-3.R
-# bwsubmit r ebf-gsk-10-4.R
-# bwsubmit r ebf-gsk-10-5.R
-# bwsubmit r ebf-gsk-15-1.R
-# bwsubmit r ebf-gsk-15-2.R
-# bwsubmit r ebf-gsk-15-3.R
-# bwsubmit r ebf-gsk-15-4.R
-# bwsubmit r ebf-gsk-15-5.R
-# bwsubmit r ebf-gsk-20-1.R
-# bwsubmit r ebf-gsk-20-2.R
-# bwsubmit r ebf-gsk-20-3.R
-# bwsubmit r ebf-gsk-20-4.R
-# bwsubmit r ebf-gsk-20-5.R
-# bwsubmit r ebf-gsk-25-1.R
-# bwsubmit r ebf-gsk-25-2.R
-# bwsubmit r ebf-gsk-25-3.R
-# bwsubmit r ebf-gsk-25-4.R
-# bwsubmit r ebf-gsk-25-5.R
-# bwsubmit r ebf-gsk-30-1.R
-# bwsubmit r ebf-gsk-30-2.R
-# bwsubmit r ebf-gsk-30-3.R
-# bwsubmit r ebf-gsk-30-4.R
-# bwsubmit r ebf-gsk-30-5.R
-bwsubmit r ebf-gsk-35-1.R
-bwsubmit r ebf-gsk-35-2.R
-bwsubmit r ebf-gsk-35-3.R
-bwsubmit r ebf-gsk-35-4.R
-bwsubmit r ebf-gsk-35-5.R
-bwsubmit r ebf-gsk-40-1.R
-bwsubmit r ebf-gsk-40-2.R
-bwsubmit r ebf-gsk-40-3.R
-bwsubmit r ebf-gsk-40-4.R
-bwsubmit r ebf-gsk-40-5.R
-# bwsubmit r gsk-gsk-5-1.R
-# bwsubmit r gsk-gsk-5-2.R
-# bwsubmit r gsk-gsk-5-3.R
-# bwsubmit r gsk-gsk-5-4.R
-# bwsubmit r gsk-gsk-5-5.R
-# bwsubmit r gsk-gsk-10-1.R
-# bwsubmit r gsk-gsk-10-2.R
-# bwsubmit r gsk-gsk-10-3.R
-# bwsubmit r gsk-gsk-10-4.R
-# bwsubmit r gsk-gsk-10-5.R
-# bwsubmit r gsk-gsk-15-1.R
-# bwsubmit r gsk-gsk-15-2.R
-# bwsubmit r gsk-gsk-15-3.R
-# bwsubmit r gsk-gsk-15-4.R
-# bwsubmit r gsk-gsk-15-5.R
-# bwsubmit r gsk-gsk-20-1.R
-# bwsubmit r gsk-gsk-20-2.R
-# bwsubmit r gsk-gsk-20-3.R
-# bwsubmit r gsk-gsk-20-4.R
-# bwsubmit r gsk-gsk-20-5.R
-# bwsubmit r gsk-gsk-25-1.R
-# bwsubmit r gsk-gsk-25-2.R
-# bwsubmit r gsk-gsk-25-3.R
-# bwsubmit r gsk-gsk-25-4.R
-# bwsubmit r gsk-gsk-25-5.R
-# bwsubmit r gsk-gsk-30-1.R
-# bwsubmit r gsk-gsk-30-2.R
-# bwsubmit r gsk-gsk-30-3.R
-# bwsubmit r gsk-gsk-30-4.R
-# bwsubmit r gsk-gsk-30-5.R
-bwsubmit r gsk-gsk-35-1.R
-bwsubmit r gsk-gsk-35-2.R
-bwsubmit r gsk-gsk-35-3.R
-bwsubmit r gsk-gsk-35-4.R
-bwsubmit r gsk-gsk-35-5.R
-bwsubmit r gsk-gsk-40-1.R
-bwsubmit r gsk-gsk-40-2.R
-bwsubmit r gsk-gsk-40-3.R
-bwsubmit r gsk-gsk-40-4.R
-bwsubmit r gsk-gsk-40-5.R
+
+for i in `seq 1 6`
+do
+  for j in `seq 1 5`
+  do
+    L=$((i * 5))
+    bwsubmit exec launch-ebf.sh $L $j
+    bwsubmit exec launch-gsk.sh $L $j
+  done
 done
+exit 0
--- markdown/fire-analysis/launch2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# CWD=`pwd`
-bwsubmit r gsk-gsk-all-1.R
-bwsubmit r gsk-gsk-all-2.R
-bwsubmit r gsk-gsk-all-3.R
-bwsubmit r gsk-gsk-all-4.R
-bwsubmit r gsk-gsk-all-5.R
-done

bfdbd58f6e8631678aedb34fb60a509c935bd758
Sam Morris <sam.morris81@gmail.com>
2016-07-27 16:40:26 -0500

    rerunning analysis

--- markdown/precipitation/launch-gsk.sh
+++ markdown/precipitation/launch-gsk.sh
@@ -5,6 +5,7 @@ PREC=$HOME/repos-git/extreme-decomp/markdown/precipitation
 L=$1
 CV=$2
 
+echo "Knots: $L, CV: $CV"
 $RBIN CMD BATCH --vanilla --no-save $PREC/fit-gsk-$L-$CV.R $PREC/fit-gsk-$L-$CV.out 2>&1
 
 exit 0

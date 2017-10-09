3d4d0cfe82f2ba2cdea0a97c20456c29e243013d
Sam Morris <sam.morris81@gmail.com>
2017-10-09 11:20:04 -0500

    Updates to run on GCE
    Running on GCE instead of beowulf. Tweaking the scripts to run across all cross-validation sets as well as garbage collect after a run to help on memory size.

--- markdown/precip-rev/fit-gsk-25.R
+++ markdown/precip-rev/fit-gsk-25.R
@@ -5,12 +5,14 @@ margin  <- "gsk"      # ebf: empirical basis functions, gsk: gaussian kernels
 for (cv in cvs) {
   # don't lose which setting we're running
   rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
+  gc()
   source(file = "./package_load.R", chdir = T)
   time    <- "current"  # current or future
   source(file = "./fitmodel.R")
   
   # don't lose which setting we're running
   rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
+  gc()
   source(file = "./package_load.R", chdir = T)
   # fit the model and get predictions
   source(file = "./fitmodel.R")

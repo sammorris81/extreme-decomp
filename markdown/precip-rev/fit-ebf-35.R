87501f792834c5037fba01d4f777015440adfc2b
Sam Morris <sam.morris81@gmail.com>
2017-10-09 11:16:29 -0500

    change to run all cv sets from a single file

---
+++ markdown/precip-rev/fit-ebf-35.R
@@ -0,0 +1,17 @@
+L <- 35               # number of knots to use for the basis functions
+cvs <- seq_len(5)     # cross-validation sets
+process <- "ebf"      # ebf: empirical basis functions, gsk: gaussian kernels
+margin  <- "gsk"      # ebf: empirical basis functions, gsk: gaussian kernels
+for (cv in cvs) {
+  # don't lose which setting we're running
+  rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
+  source(file = "./package_load.R", chdir = T)
+  time    <- "current"  # current or future
+  source(file = "./fitmodel.R")
+  
+  # don't lose which setting we're running
+  rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
+  source(file = "./package_load.R", chdir = T)
+  # fit the model and get predictions
+  source(file = "./fitmodel.R")
+}

e7f20b7b06907eb10854f0b7c5f9d16037405ce4
Sam Morris <sam.morris81@gmail.com>
2017-10-11 10:26:17 -0500

    bugfix keep times too

--- markdown/precip-rev/fit-ebf-30.R
+++ markdown/precip-rev/fit-ebf-30.R
@@ -6,7 +6,8 @@ times <- c("current", "future")
 for (cv in cvs) {
   for (time in times) {
     # don't lose which setting we're running
-    rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin", "time")))
+    rm(list=setdiff(ls(), 
+                    c("cv", "cvs", "L", "process", "margin", "time", "times")))
     gc()
     source(file = "./package_load.R", chdir = T)
     source(file = "./fitmodel.R")

c81a511eb4d2c22c53205c4de0bc70f1dbf66b87
Sam Morris <sam.morris81@gmail.com>
2017-10-11 10:21:46 -0500

    bugfix to include time for future

--- markdown/precip-rev/fit-ebf-20.R
+++ markdown/precip-rev/fit-ebf-20.R
@@ -2,18 +2,13 @@ L <- 20               # number of knots to use for the basis functions
 cvs <- seq_len(5)     # cross-validation sets
 process <- "ebf"      # ebf: empirical basis functions, gsk: gaussian kernels
 margin  <- "gsk"      # ebf: empirical basis functions, gsk: gaussian kernels
+times <- c("current", "future")
 for (cv in cvs) {
-  # don't lose which setting we're running
-  rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
-  gc()
-  source(file = "./package_load.R", chdir = T)
-  time    <- "current"  # current or future
-  source(file = "./fitmodel.R")
-  
-  # don't lose which setting we're running
-  rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin")))
-  gc()
-  source(file = "./package_load.R", chdir = T)
-  # fit the model and get predictions
-  source(file = "./fitmodel.R")
+  for (time in times) {
+    # don't lose which setting we're running
+    rm(list=setdiff(ls(), c("cv", "cvs", "L", "process", "margin", "time")))
+    gc()
+    source(file = "./package_load.R", chdir = T)
+    source(file = "./fitmodel.R")
+  }
 }

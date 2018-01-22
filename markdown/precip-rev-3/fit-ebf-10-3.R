d510aac18fc68a6490036cf55862f5150baf0fb9
Sam Morris <sam.morris81@gmail.com>
2018-01-22 12:13:19 -0800

    adding in calls to fit for rest of L options

---
+++ markdown/precip-rev-3/fit-ebf-10-3.R
@@ -0,0 +1,16 @@
+L <- 10               # number of knots to use for the basis functions
+cv <- 3
+cvs <- seq_len(5)     # cross-validation sets
+process <- "ebf"      # ebf: empirical basis functions, gsk: gaussian kernels
+margin  <- "gsk"      # ebf: empirical basis functions, gsk: gaussian kernels
+times <- c("current", "future")
+# for (cv in cvs) {
+  for (time in times) {
+    # don't lose which setting we're running
+    rm(list=setdiff(ls(), 
+                    c("cv", "cvs", "L", "process", "margin", "time", "times")))
+    gc()
+    source(file = "./package_load.R", chdir = T)
+    source(file = "./fitmodel.R")
+  }
+# }
